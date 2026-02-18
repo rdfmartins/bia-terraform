737f479dd7e216f05b3cb5ece111c46b75dee32a
Rodolfo Martins <bonquers@gmai.com>
2026-02-17 21:31:15 -0300

    feat: add AWS provider and EC2 instance resource to main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,32 @@
 terraform {
+  # Versão do Terraform
    required_version = "~> 1.14.0" 
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      # Versão do Provedor AWS (quem entende os comandos da Amazon)
+      #versão do provider aws
       version = "~> 6.0" 
     }
   }
 }
 
+#provider do tipo aws
 provider "aws" {
   region  = "us-east-1"
   profile = "rodolfo"
+}
+
+#recurso do tipo aws_instance
+#criando uma instância EC2
+resource "aws_instance" "bia-dev" {
+  ami           = "ami-02f3f602d23f1659d"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "bia-dev-ec2"
+    Environment = "dev"
+    Owner = "Rodolfo"
+    Project = "bia-terraform"
+    Created_by = "Terraform"
+      }
 }
